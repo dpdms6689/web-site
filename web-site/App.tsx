8633def219a4deed1116792fe6b647c8cd056432
dpdms6689 <127912008+dpdms6689@users.noreply.github.com>
2024-11-04 09:48:24 +0900

    Add files via upload

---
+++ web-site/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Route, Routes, Navigate } from "react-router-dom";
+import Router from "components/Router";
+
+function App() {
+  return <Router/>;
+}
+
+export default App;
